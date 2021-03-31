c58100a0a0613188c41f92463decc0d2d9caf2a9
Jasim Gazi <Jasimakram96@gmail.com>
2021-03-31 19:23:21 +0530

    Arts

--- tabs/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express();
-const port = 3200;
-const serveStatic = require('serve-static');
-
-
-app.use(serveStatic('public', { 'index': ['index.html', 'index.htm'] }))
-app.listen(port)
